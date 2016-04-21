c807759a9a1034e178286bcbc3c57633e2d20123
Gabrielle <gabivk@gmail.com>
2016-04-21 13:10:27 +0100

    adds avatar to UserFactory

--- app/js/factories/UserFactory.js
+++ app/js/factories/UserFactory.js
@@ -1,6 +1,7 @@
 gitHubUserList.factory('UserFactory', function() {
-    var user = function(username){
+    var user = function(username, avatar){
       this.username = username;
+      this.avatar   = avatar;
     };
     return user;
 });
--- test/unit/userFactory.spec.js
+++ test/unit/userFactory.spec.js
@@ -1,14 +1,20 @@
 describe('userFactory', function(){
   beforeEach(module('gitHubUserList'));
 
-  var user;
+  var user, username, avatar;
 
   beforeEach(inject(function(UserFactory){
-    var username = 'Bob';
-    user = new UserFactory(username);
+    username = 'Bob';
+    avatar = 'https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png';
+    user = new UserFactory(username, avatar);
   }));
 
   it('is expected to have a username', function(){
-    expect(user.username).toEqual('Bob');
+      expect(user.username).toEqual(username);
   });
+
+  it('is expected to have an avatar', function(){
+    expect(user.avatar).toEqual(avatar);
+  });
+
 });

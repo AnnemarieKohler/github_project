bb13ed8be28192eead24c9eed8bd206a0dd1f272
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-21 13:20:14 +0100

    user has followers

--- app/js/factories/UserFactory.js
+++ app/js/factories/UserFactory.js
@@ -1,7 +1,8 @@
 gitHubUserList.factory('UserFactory', function() {
-    var user = function(username, avatar){
+    var user = function(username, avatar, followers){
       this.username = username;
       this.avatar   = avatar;
+      this.followers = followers;
     };
     return user;
 });
--- test/unit/userFactory.spec.js
+++ test/unit/userFactory.spec.js
@@ -1,12 +1,13 @@
 describe('userFactory', function(){
   beforeEach(module('gitHubUserList'));
 
-  var user, username, avatar;
+  var user, username, avatar, followers;
 
   beforeEach(inject(function(UserFactory){
     username = 'Bob';
     avatar = 'https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png';
-    user = new UserFactory(username, avatar);
+    followers = 10;
+    user = new UserFactory(username, avatar, followers);
   }));
 
   it('is expected to have a username', function(){
@@ -15,6 +16,10 @@ describe('userFactory', function(){
 
   it('is expected to have an avatar', function(){
     expect(user.avatar).toEqual(avatar);
+  });
+
+  it('is expected to have followers', function(){
+    expect(user.followers).toEqual(followers);
   });
 
 });

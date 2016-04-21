6997b71943f3b8b07706291fead45c541cff2e47
Claudia Green <claudiagreen@Claudias-MacBook-Pro-2.local>
2016-04-21 15:58:09 +0100

    Edited index

--- app/js/controllers/userListController.js
+++ app/js/controllers/userListController.js
@@ -1,10 +1,12 @@
 gitHubUserList.controller('gitHubUserController', ['UserFactory', function(UserFactory) {
   var self = this;
   this.users=[{username: "claudia", avatar: "testurl", followers: "1000", repos: "1"},
-              {username: "andrew", avatar: "testurl", followers: "1", repos: "5"}]
+              {username: "andrew", avatar: "testurl", followers: "1", repos: "5"},
+              {username: "lorenzo", avatar: "testurl", followers: "100", repos: "512"},
+              {username: "poo", avatar: "testurl", followers: "121", repos: "95"}]
 
-  self.searchUsers = function(searchTerm) {
-  };
+  // self.searchUsers = function(searchTerm) {
+  // };
 
 
 }]);

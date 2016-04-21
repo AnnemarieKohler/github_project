ef58e28260f532de69158d6f427574d2ed37e779
Claudia Green <claudiagreen100@gmail.com>
2016-04-21 16:04:21 +0100

    Update userListController.js

--- app/js/controllers/userListController.js
+++ app/js/controllers/userListController.js
@@ -3,7 +3,7 @@ gitHubUserList.controller('gitHubUserController', ['UserFactory', function(UserF
   this.users=[{username: "claudia", avatar: "testurl", followers: "1000", repos: "1"},
               {username: "andrew", avatar: "testurl", followers: "1", repos: "5"},
               {username: "lorenzo", avatar: "testurl", followers: "100", repos: "512"},
-              {username: "poo", avatar: "testurl", followers: "121", repos: "95"}]
+              {username: "person", avatar: "testurl", followers: "121", repos: "95"}]
 
   // self.searchUsers = function(searchTerm) {
   // };

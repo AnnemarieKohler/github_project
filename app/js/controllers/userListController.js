0600de1babb26671767a8008ebdfa878e7933ac6
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-22 15:16:53 +0100

    Remove debugging banana

--- app/js/controllers/userListController.js
+++ app/js/controllers/userListController.js
@@ -1,7 +1,7 @@
 gitHubUserList.controller('gitHubUserController', ['UserListService', function(UserListService) {
   var self = this;
 
-  self.users = ["banana"];
+  self.users = [];
 
   UserListService.getAll()
     .then(function(list) {
